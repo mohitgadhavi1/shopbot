bd8d7b22f4510070ce1251553f14f30afea177c0
Mohit Gadhavi <mohitgadhavi1@gmail.com>
2025-06-09 09:20:29 +0530

    permission granted for new users

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignupSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
 class SignupView(APIView):
+    permission_classes = [AllowAny] 
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
